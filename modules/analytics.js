9b6798f2344718494c0311067ef55cb37dc1ce3e
wingsling0531 <47451186+wingsling0531@users.noreply.github.com>
2019-02-08 21:25:49 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -2,6 +2,7 @@ var Sample = require("../mainFunc/Sample.js");
 
 function parseInput(inputStr,UserID,UserN,GroupID) {
 	if (inputStr.match(/^嗨$/) != null) return Sample.Hello();//哈喽
+	else if (inputStr.match(/^奶綠$/) != null) return Sample.greentea();//是隻鳥
 	else return '';
 }
 
